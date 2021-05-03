110348710017f1163efe4964cf5671d236b75e2c
carinagmorais <carinagmorais@gmail.com>
2021-05-03 15:31:07 +0200

    Added playSound Function

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-window.addEventListener(`keydown`, function (e) {
-  console.log(e);
-});
+function playSound(e) {
+  let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+
+  key.classList.add("playing");
+  audio.currentTime = 0;
+  audio.play();
+}
+
+window.addEventListener("keydown", playSound);
